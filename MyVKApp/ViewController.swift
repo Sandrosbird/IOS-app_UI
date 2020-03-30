c0c05033bf5ee250b14885cac461c64f9297f48b
Emil Mescheryakov <emil.sam12@gmail.com>
2020-03-30 11:46:43 +0300

    no message

--- MyVKApp/ViewController.swift
+++ MyVKApp/ViewController.swift
@@ -14,18 +14,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var appNameLabel: UILabel!
     @IBOutlet weak var loginLabel: UILabel!
     @IBOutlet weak var passwordLabel: UILabel!
-    @IBOutlet weak var loadScreenButton: UIButton!
     @IBOutlet weak var scrollBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var loginTextFIeld: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+
+    
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        scrollBottomConstraint.constant = 500
+        
         appNameLabel.text = "MyVKApp"
         loginLabel.text = "Login"
         passwordLabel.text = "Password"
+        
         
         NotificationCenter.default.addObserver(
             self,
@@ -33,22 +40,43 @@ class ViewController: UIViewController {
             name: UIResponder.keyboardWillShowNotification,
             object: nil)
         NotificationCenter.default.addObserver(
-        self,
-        selector: #selector(keyboardWillBeHidden(notification:)),
-        name: UIResponder.keyboardWillHideNotification,
-        object: nil)
+            self,
+            selector: #selector(keyboardWillBeHidden(notification:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
     }
+    
+    @IBAction func logout(unwindSegue: UIStoryboardSegue) {}
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
     
-    @IBAction func loginPressed(_ sender: UIButton, forEvent: UIEvent) {
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        
+        let login = loginTextFIeld.text!
+        let password = passwordTextField.text!
+        
+        if login == "admin" && password == "123456" {
+            return true
+        } else {
+            let alert = UIAlertController(title: "Ошибка", message: "Логин или пароль введены неверно", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+            return false
+        }
     }
     
+   
+    
     @objc func keyboardWasShown (notification: Notification) {
-        //нижний отступ UIScrollView по размеру клавиатуры
-        scrollBottomConstraint.constant = 300
+        let userInfo = (notification as NSNotification).userInfo as! [String: Any]
+        let frame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+        
+        scrollBottomConstraint.constant = frame.height
+        
     }
     
     @objc func keyboardWillBeHidden(notification: Notification) {
